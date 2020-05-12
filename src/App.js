57a746616c64c897845fc8811e028f82918ccb2d
Rajath Bharadwaj <rajathb13@gmail.com>
2020-05-12 17:21:15 +0530

    First commit
    @mohit9222

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
+// styles
+import './index.scss';
+
+// components
+import Home from './pages/Home';
+import Topics from './pages/Topics'
+import Header from './components/Header';
+import SidebarNav from './components/SidebarNav';
+
+const App = () => {
+  let [menuState, setMenuState] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App container">
+        <Header menuState={menuState} setMenuState={setMenuState} />
+        <SidebarNav menuState={menuState} setMenuState={setMenuState} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route exact path="/Topics" component={Topics} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+/**
+ * Header Component should hold the menu trigger button.
+ * onClick of this button we need to show or hide the sidebar navigation
+ * The Menu Toggle state is maintained in the App Component.
+ */
+const Header = (props) => {
+  const { menuState, setMenuState } = props;
+  return (
+    <header className="site-header">
+      <div className="brand-icon">
+        <div className="icon">
+          <i className="fab fa-react"></i>
+        </div>
+        <span>FrontendFunn</span>
+      </div>
+      <div>
+        <button
+          className={`menu-trigger ${menuState ? "menu-close" : ""}`}
+          onClick={() => setMenuState(!menuState)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+    </header>
+  );
+};
+export default Header;
